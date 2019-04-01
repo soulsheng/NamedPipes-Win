778eea9ba66cf66bc0943991c60c89efb3709fb9
s <s>
2019-04-02 00:14:44 +0800

    buffer size 16M, at 20fps

--- Examples/example.cpp
+++ Examples/example.cpp
@@ -2,17 +2,20 @@
 #include <IPCClient.h>
 #include <stdio.h>
 
+#define		SIZE_BUFFER	1024*1024*4*4
+#define		TIME_INTERVAL	50
+
 class ServerHandler : public IPCMessageHandler
 {
 public:
 	virtual void OnPacketReceived(IIPC* IPC, char *szBuff, unsigned long length)
 	{
 		printf("Received[%d]: %s\n", length, szBuff);
-		char szBuffer[128];
-		sprintf_s(szBuffer, "Server Msg");
+		sprintf_s(szBuffer, SIZE_BUFFER, "Server Msg");
 		IPC->Send(szBuffer, strlen(szBuffer) + 1);
 		
 	}
+	char szBuffer[SIZE_BUFFER];
 };
 
 
@@ -22,11 +25,11 @@ public:
 	virtual void OnPacketReceived(IIPC* IPC, char *szBuff, unsigned long length)
 	{
 		printf("Received[%d]: %s\n", length, szBuff);
-		char szBuffer[128];
-		sprintf_s(szBuffer, "Client Msg");
+		sprintf_s(szBuffer, SIZE_BUFFER, "Client Msg");
 		IPC->Send(szBuffer, strlen(szBuffer) + 1);
 		
 	}
+	char szBuffer[SIZE_BUFFER];
 };
 
 int main(int argc, char *argv[])
@@ -40,11 +43,11 @@ int main(int argc, char *argv[])
 		printf("Waiting for incoming connections!\n");
 		while (1)
 		{
-			while (server->PeekMessages(100))
+			while (server->PeekMessages(TIME_INTERVAL))
 			{
 				
 			}
-			Sleep(100);
+			Sleep(TIME_INTERVAL);
 		}
 	}
 	else{
@@ -66,11 +69,11 @@ int main(int argc, char *argv[])
 		}
 		while (1)
 		{
-			while (client->PeekMessages(100))
+			while (client->PeekMessages(TIME_INTERVAL))
 			{		
 				printf("===========\n");
 			}
-			Sleep(100);
+			Sleep(TIME_INTERVAL);
 		}
 
 	}
